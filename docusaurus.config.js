7b1550e899ee714c51db04bcced630e78fc69ff2
Donwauchopex <donwauchopex@gmail.com>
2022-08-23 17:38:18 +0200

    Add edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,6 +34,7 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/operandinc/docs/tree/trunk",
           routeBasePath: "/",
         },
 
